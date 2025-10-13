5d533ff19d666b1ed66deb833e947b0a9772db5c
Yanila Soliz <yanilasoliz23@gmail.com>
2025-10-12 20:28:40 -0400

    implementacion de los controladores

---
+++ app/Http/Controllers/FestividadController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Festividad;
+use Illuminate\Http\Request;
+
+class FestividadController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Festividad $festividad)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Festividad $festividad)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Festividad $festividad)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Festividad $festividad)
+    {
+        //
+    }
+}
