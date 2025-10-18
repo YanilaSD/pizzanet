083e4df7da8fc7f47fea7b0d5dad5fb1b17bf768
Yanila Soliz <yanilasoliz23@gmail.com>
2025-10-18 16:37:43 -0400

    funcionalidad privilegios

---
+++ resources/views/modules/privilegios/index.blade.php
@@ -0,0 +1,96 @@
+<x-layouts.app>
+    <div class="flex justify-between">
+        <div class="">
+            <h1 class=" text-2xl">Gestión de Privilegios</h1>
+            <p>Registra y gestiona todas los privilegios del sistema</p>
+        </div>
+        <flux:button href="{{ route('privilegios.create') }}">
+            + Nueva Privilegios
+        </flux:button>
+
+    </div>
+
+    @if (session('success'))
+        <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative my-4" role="alert">
+            <strong class="font-bold">¡Éxito!</strong>
+            <span class="block sm:inline">{{ session('success') }}</span>
+        </div>
+    @endif
+
+
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg mt-8">
+        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 mb-2.5">
+            <caption
+                class="p-5 text-lg font-semibold text-left rtl:text-right text-gray-900 bg-white dark:text-white dark:bg-gray-800">
+
+                {{-- Revisa CSS --}}
+                <div class="row justify-between">
+                    <div class="">
+                        Registro de Privilegios
+                        <p class="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400 mb-2.5">Listado de los
+                            privilegios
+                            registrados</p>
+                    </div>
+                    <form method="GET" action="{{ route('privilegios.index') }}">
+                        <flux:input name="search" icon="magnifying-glass" placeholder="Buscar privilegio"
+                            value="{{ request('search') }}" />
+                    </form>
+                </div>
+
+
+            </caption>
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th scope="col" class="px-6 py-3">
+                        #
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Nombre
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Descripcion
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Estado
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Acciones
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($privilegios as $privilegio)
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                        <th scope="row"
+                            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {{ $loop->index + 1 }}
+                        </th>
+                        <td class="px-6 py-4">
+                            {{ $privilegio->nombre }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ $privilegio->descripcion }}
+                        </td>
+                        <td class="px-6 py-4">
+                            <flux:badge color="{{ $privilegio->estado == '1' ? 'green' : 'red' }}">
+                                {{ $privilegio->estado == '1' ? 'Activo' : 'Inactivo' }}
+                            </flux:badge>
+
+                        </td>
+                        <td class="px-6 py-4 text-right flex gap-2.5">
+                            <a href="{{ route('privilegios.edit', $privilegio) }}" class="text-white dark:text-gray-500 hover:underline mr-4">
+                                <flux:icon name="pencil-square" />
+                            </a>
+                            <a href="{{ route('privilegios.toggle', $privilegio->id) }}"
+                                class="font-medium text-white dark:text-gray-500 hover:underline flex justify-center justify-items-center gap-1">
+                                <flux:icon name="{{ $privilegio->estado == '0' ? 'check-circle' : 'no-symbol' }}" />
+                            </a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+        {{ $privilegios->links() }}
+    </div>
+
+</x-layouts.app>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Laravel\Fortify\Features;
 use Livewire\Volt\Volt;
 
 use App\Http\Controllers\VentaController;
+use App\Http\Controllers\PrivilegioController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,6 +19,15 @@ Route::middleware(['auth'])->group(function () {
     Route::redirect('settings', 'settings/profile');
 
     Route::get('/ventas', [VentaController::class, 'index'])->name('ventas.index');
+
+    //Privilegio
+    Route::get('/privilegios', [PrivilegioController::class, 'index'])->name('privilegios.index');
+    Route::get('/privilegios/create', [PrivilegioController::class, 'create'])->name('privilegios.create');
+    Route::post('/privilegios', [PrivilegioController::class, 'store'])->name('privilegios.store');
+    Route::get('/privilegios/{privilegio}/edit', [PrivilegioController::class, 'edit'])->name('privilegios.edit');
+    Route::put('/privilegios/{privilegio}', [PrivilegioController::class, 'update'])->name('privilegios.update');
+    Route::get('/privilegios/{id}/toggle', [PrivilegioController::class, 'toggle'])->name('privilegios.toggle');
+
 
     Volt::route('settings/profile', 'settings.profile')->name('profile.edit');
     Volt::route('settings/password', 'settings.password')->name('password.edit');
