86d339c678f942065ebe4e154d60611d7e204fbf
Yanila Soliz <solizyanila23@gmail.com>
2026-03-01 17:06:48 -0400

    correcion de layout livewire

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Livewire\Volt\Volt;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        view()->addNamespace('layouts', resource_path('views/components/layouts'));
+        Volt::mount([
+            resource_path('views/livewire'),
+            resource_path('views/pages'),
+        ]);
     }
 }
